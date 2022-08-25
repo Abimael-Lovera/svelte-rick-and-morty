d07dfa18f2fba1329b5447bdfe4e746afc35a1bf
Abimael-Lovera <abimaellovera@gmail.com>
2022-08-24 21:16:35 -0300

    paginacion

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,31 @@
 <script>
-	import { each } from 'svelte/internal';
-
 	let characters = [];
+	let page = 1;
 	async function loadCharacters() {
-		const response = await fetch('https://rickandmortyapi.com/api/character');
+		const response = await fetch(
+			`https://rickandmortyapi.com/api/character?page=${page}`
+		);
 		const data = await response.json();
 		characters = data.results;
 	}
 	loadCharacters();
+
+	function nextPage() {
+		page++;
+		loadCharacters();
+	}
+
+	function previousPage() {
+		page--;
+		loadCharacters();
+	}
 </script>
 
 <h1>API Rick and Morty Svelte</h1>
 
 <div>
-	<button>previous</button>
-	<button>next</button>
+	<button on:click={previousPage} disabled={page === 1}>previous</button>
+	<button on:click={nextPage}>next</button>
 </div>
 
 <div class="">
