b85f791b162c9af38c4fdc2a5a4161186d05267d
Abimael-Lovera <abimaellovera@gmail.com>
2022-08-24 21:00:59 -0300

    consumindo api com fetch

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,28 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import { each } from 'svelte/internal';
+
+	let characters = [];
+	async function loadCharacters() {
+		const response = await fetch('https://rickandmortyapi.com/api/character');
+		const data = await response.json();
+		characters = data.results;
+	}
+	loadCharacters();
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<h1>API Rick and Morty Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+<div>
+	<button>previous</button>
+	<button>next</button>
+</div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<div class="">
+	{#each characters as character}
+		<div>
+			<img src={character.image} alt={character.name} />
+			<h2>{character.name}</h2>
+			<h3>{character.species}</h3>
+		</div>
+	{/each}
+</div>
